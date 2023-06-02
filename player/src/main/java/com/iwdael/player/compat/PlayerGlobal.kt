1dbc11acd7381b157574132f67fa8679e59d2d44
iwdael <iwdael@outlook.com>
2023-06-02 22:05:05 +0800

    fix bug

--- player/src/main/java/com/iwdael/player/compat/PlayerGlobal.kt
+++ player/src/main/java/com/iwdael/player/compat/PlayerGlobal.kt
@@ -3,7 +3,6 @@ package com.iwdael.player.compat
 import android.app.Activity
 import android.app.Application
 import android.content.Context
-import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,10 +11,6 @@ import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import com.iwdael.player.Engine
-import com.iwdael.player.EngineCreator
-import com.iwdael.player.ThumbnailLoader
-import com.iwdael.player.default.DefaultEngineCreator
-import com.iwdael.player.default.DefaultThumbnailLoader
 
 /**
  * author : Iwdael
@@ -25,51 +20,59 @@ import com.iwdael.player.default.DefaultThumbnailLoader
  * version: 1.0
  */
 
-val mainHandler by lazy { Handler(Looper.getMainLooper()) }
-fun runMain(call: () -> Unit) = mainHandler.post { call.invoke() }
-val activityLifecycleCallbacks = ArrayList<Pair<Activity, Application.ActivityLifecycleCallbacks>>()
-val engineContainer = hashMapOf<String, Engine>()
-var registeredApplicationLifecycleCallbacks = false
-val applicationLifecycleCallbacks = object :
+internal val mainHandler by lazy { Handler(Looper.getMainLooper()) }
+internal fun runMain(call: () -> Unit) = mainHandler.post { call.invoke() }
+internal val activityLifecycleCallbacks =
+    ArrayList<Pair<Activity, Application.ActivityLifecycleCallbacks>>()
+internal val engineContainer = hashMapOf<String, Engine>()
+internal var registeredApplicationLifecycleCallbacks = false
+internal val applicationLifecycleCallbacks = object :
     Application.ActivityLifecycleCallbacks {
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-        activityLifecycleCallbacks.forEach {
+        val copy = activityLifecycleCallbacks.toList()
+        copy.forEach {
             if (it.first == activity) it.second.onActivityCreated(activity, savedInstanceState)
         }
     }
 
     override fun onActivityStarted(activity: Activity) {
-        activityLifecycleCallbacks.forEach {
+        val copy = activityLifecycleCallbacks.toList()
+        copy.forEach {
             if (it.first == activity) it.second.onActivityStarted(activity)
         }
     }
 
     override fun onActivityResumed(activity: Activity) {
-        activityLifecycleCallbacks.forEach {
+        val copy = activityLifecycleCallbacks.toList()
+        copy.forEach {
             if (it.first == activity) it.second.onActivityResumed(activity)
         }
     }
 
     override fun onActivityPaused(activity: Activity) {
-        activityLifecycleCallbacks.forEach {
+        val copy = activityLifecycleCallbacks.toList()
+        copy.forEach {
             if (it.first == activity) it.second.onActivityPaused(activity)
         }
     }
 
     override fun onActivityStopped(activity: Activity) {
-        activityLifecycleCallbacks.forEach {
+        val copy = activityLifecycleCallbacks.toList()
+        copy.forEach {
             if (it.first == activity) it.second.onActivityStopped(activity)
         }
     }
 
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-        activityLifecycleCallbacks.forEach {
+        val copy = activityLifecycleCallbacks.toList()
+        copy.forEach {
             if (it.first == activity) it.second.onActivitySaveInstanceState(activity, outState)
         }
     }
 
     override fun onActivityDestroyed(activity: Activity) {
-        activityLifecycleCallbacks.forEach {
+        val copy = activityLifecycleCallbacks.toList()
+        copy.forEach {
             if (it.first == activity) it.second.onActivityDestroyed(activity)
         }
     }
